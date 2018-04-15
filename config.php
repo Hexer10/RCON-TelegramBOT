f0d18a449e12bae643bef1aa2ef3f37ae5d9099c
Hexah <hexer504@gmail.com>
2018-04-15 15:52:34 +0200

    Add files

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+	//BOT Settings
+	$token = '560142313:AAGgLrtxTgB_cMBr0ZNUDwFg_Q6FGN69Fuo'; //Telegram BOT Token
+
+	//MySQL Settings
+	$host = "localhost"; //DB Host
+	$name = "root"; //DB User
+	$password = ""; //DB Password
+	$database = "telegram"; //DB Name
+?>
